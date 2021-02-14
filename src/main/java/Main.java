dfae9485c6b3765e06ffe95368f39c2cbaa2fbb7
ED04_JB <cosomartoscarlosdel@gmail.com>
2021-02-14 13:27:23 +0100

    El saldo actual es2500.0 Ingreso en cuenta

---
+++ src/main/java/Main.java
@@ -0,0 +1,43 @@
+
+import Cuentas.CCuenta;
+
+/**
+ *
+ * @author Admin
+ */
+public class Main {
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+      OperativaCuenta();  
+    }
+    
+    /**
+     *
+     */
+    public static void OperativaCuenta(){
+              CCuenta cuenta1;
+        double saldoActual;
+        float cantidad;
+        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es"+ saldoActual );
+
+        try {
+            cuenta1.retirar(2300);
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            cuenta1.ingresar(695);
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }  
+        
+        
+    }
+}
